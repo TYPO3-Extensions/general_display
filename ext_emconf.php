3565b0746a0d15a86a377b4c920cb2c4b147d991
nelsonglory <nelsonglory@735d13b6-9817-0410-8766-e36946ffe9aa>
2015-07-17 14:47:45 +0000

    maintanance release
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/general_display/tags/1.6.1@89494 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,50 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "general_data_display".
+ *
+ * Auto generated 13-11-2013 14:00
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'General data display',
+	'description' => 'This plugin can be used to display any categorizable data. It creates a link list out of your titles and categories which points to detail views of your defined data fields.',
+	'category' => 'plugin',
+	'shy' => 0,
+	'version' => '1.6.0',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'beta',
+	'uploadfolder' => 1,
+	'createDirs' => 'uploads/tx_generaldatadisplay',
+	'modify_tables' => '',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'Roderick Braun',
+	'author_email' => 'roderick.braun@ph-freiburg.de',
+	'author_company' => 'University of Education Freiburg',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '4.5.0-6.2.99',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:39:{s:9:"ChangeLog";s:4:"45d0";s:20:"class.ext_update.php";s:4:"ffd0";s:12:"ext_icon.gif";s:4:"790c";s:17:"ext_localconf.php";s:4:"649c";s:14:"ext_tables.php";s:4:"ce8b";s:14:"ext_tables.sql";s:4:"72d4";s:19:"flexform_ds_pi1.xml";s:4:"51cd";s:41:"icon_tx_generaldatadisplay_categories.gif";s:4:"475a";s:35:"icon_tx_generaldatadisplay_data.gif";s:4:"475a";s:41:"icon_tx_generaldatadisplay_datafields.gif";s:4:"475a";s:13:"locallang.xml";s:4:"d1a8";s:16:"locallang_db.xml";s:4:"2431";s:10:"README.txt";s:4:"12b8";s:7:"tca.php";s:4:"de48";s:15:"css/default.css";s:4:"5385";s:14:"doc/manual.sxw";s:4:"8097";s:19:"doc/wizard_form.dat";s:4:"4405";s:20:"doc/wizard_form.html";s:4:"717c";s:22:"images/button_down.gif";s:4:"fa54";s:20:"images/button_up.gif";s:4:"0cc7";s:17:"images/create.png";s:4:"b091";s:15:"images/edit.png";s:4:"baf6";s:16:"images/reset.png";s:4:"31d1";s:17:"images/return.png";s:4:"4720";s:18:"images/tooltip.png";s:4:"d232";s:16:"images/trash.png";s:4:"a6ba";s:14:"pi1/
+ce_wiz.gif";s:4:"ac56";s:39:"pi1/class.tx_generaldatadisplay_pi1.php";s:4:"5ef9";s:50:"pi1/class.tx_generaldatadisplay_pi1_dataFields.php";s:4:"3337";s:51:"pi1/class.tx_generaldatadisplay_pi1_dataStructs.php";s:4:"385a";s:46:"pi1/class.tx_generaldatadisplay_pi1_export.php";s:4:"aedf";s:48:"pi1/class.tx_generaldatadisplay_pi1_formData.php";s:4:"48ce";s:49:"pi1/class.tx_generaldatadisplay_pi1_objClause.php";s:4:"633d";s:46:"pi1/class.tx_generaldatadisplay_pi1_objVar.php";s:4:"d358";s:49:"pi1/class.tx_generaldatadisplay_pi1_queryList.php";s:4:"eff1";s:47:"pi1/class.tx_generaldatadisplay_pi1_wizicon.php";s:4:"9406";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"9952";s:23:"templates/template.html";s:4:"7162";}',
+	'suggests' => array(
+	),
+);
+
+?>
